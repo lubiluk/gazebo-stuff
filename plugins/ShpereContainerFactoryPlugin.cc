e9a39834bf8921eee350f61c1ec911b8cfad9a5a
Pawel Gajewski <lubiluk@gmail.com>
2017-06-04 22:08:41 +0100

    Frying pan made out of spheres

--- plugins/ShpereContainerFactoryPlugin.cc
+++ plugins/ShpereContainerFactoryPlugin.cc
@@ -23,9 +23,10 @@ void ShpereContainerFactoryPlugin::Load(physics::WorldPtr _parent, sdf::ElementP
     std::stringstream xml;
 
     xml << "<sdf version ='1.6'>"
-            "<model name ='butter'>"
-            "<static>1</static>"
-            "<pose>" << poseArg << "</pose>";
+            "<model name ='sphere_container'>"
+            "<static>false</static>"
+            "<pose>" << poseArg << "</pose>"
+                "<link name='link'>";
 
 
     double sphereZ = 0.0;
@@ -41,33 +42,32 @@ void ShpereContainerFactoryPlugin::Load(physics::WorldPtr _parent, sdf::ElementP
             double sphereY = ringRadius * cos(angle);
             std::string index = std::to_string(i) + "_" + std::to_string(j);
 
-            xml << "<link name='link_" << index << "'>"
-                    "<pose>" << sphereX << " " << sphereY << " " << sphereZ << " 0 0 0</pose>"
-                        "<collision name ='collision_" << index << "'>"
-                            "<geometry>"
-                                "<sphere>"
-                                    "<radius>" << sphereRadius << "</radius>"
-                                "</sphere>"
-                            "</geometry>"
-                        "</collision>"
-                        "<visual name ='visual_" << index << "'>"
-                            "<geometry>"
-                                "<sphere>"
-                                    "<radius>" << sphereRadius << "</radius>"
-                                "</sphere>"
-                            "</geometry>"
-                            "<material>"
-                                "<script>"
-                                    "<uri>file://media/materials/scripts/gazebo.material</uri>"
-                                    "<name>Gazebo/DarkGrey</name>"
-                                "</script>"
-                            "</material>"
-                        "</visual>"
-                    "</link>";
+            xml << "<collision name ='collision_" << index << "'>"
+                        "<pose>" << sphereX << " " << sphereY << " " << sphereZ << " 0 0 0</pose>"
+                        "<geometry>"
+                            "<sphere>"
+                                "<radius>" << sphereRadius << "</radius>"
+                            "</sphere>"
+                        "</geometry>"
+                    "</collision>"
+                    "<visual name ='visual_" << index << "'>"
+                        "<pose>" << sphereX << " " << sphereY << " " << sphereZ << " 0 0 0</pose>"
+                        "<geometry>"
+                            "<sphere>"
+                            "<radius>" << sphereRadius << "</radius>"
+                            "</sphere>"
+                        "</geometry>"
+                        "<material>"
+                            "<script>"
+                                "<uri>file://media/materials/scripts/gazebo.material</uri>"
+                                "<name>Gazebo/DarkGrey</name>"
+                            "</script>"
+                        "</material>"
+                    "</visual>";
         }
     }
 
-    for (int k = 0; k < containerHeight; ++k) {
+    for (unsigned k = 0; k < containerHeight; ++k) {
         double ringRadius = sphereRadius * containerRadius;
         unsigned sphereNumber = static_cast<unsigned>(ceil(M_PI * ringRadius / sphereRadius));
         double angleStep = 2 * M_PI / sphereNumber;
@@ -80,35 +80,35 @@ void ShpereContainerFactoryPlugin::Load(physics::WorldPtr _parent, sdf::ElementP
             double sphereY = ringRadius * cos(angle);
             std::string index = "rant_" + std::to_string(k) + "_" + std::to_string(j);
 
-            xml << "<link name='link_" << index << "'>"
-                    "<pose>" << sphereX << " " << sphereY << " " << sphereZ << " 0 0 0</pose>"
-                        "<collision name ='collision_" << index << "'>"
+            xml << "<collision name ='collision_" << index << "'>"
+                        "<pose>" << sphereX << " " << sphereY << " " << sphereZ << " 0 0 0</pose>"
                         "<geometry>"
-                        "<sphere>"
-                        "<radius>" << sphereRadius << "</radius>"
-                        "</sphere>"
+                            "<sphere>"
+                                "<radius>" << sphereRadius << "</radius>"
+                            "</sphere>"
                         "</geometry>"
-                        "</collision>"
-                        "<visual name ='visual_" << index << "'>"
+                    "</collision>"
+                    "<visual name ='visual_" << index << "'>"
+                        "<pose>" << sphereX << " " << sphereY << " " << sphereZ << " 0 0 0</pose>"
                         "<geometry>"
-                        "<sphere>"
-                        "<radius>" << sphereRadius << "</radius>"
-                        "</sphere>"
+                            "<sphere>"
+                                "<radius>" << sphereRadius << "</radius>"
+                            "</sphere>"
                         "</geometry>"
                         "<material>"
-                        "<script>"
-                        "<uri>file://media/materials/scripts/gazebo.material</uri>"
-                        "<name>Gazebo/DarkGrey</name>"
-                        "</script>"
+                            "<script>"
+                                "<uri>file://media/materials/scripts/gazebo.material</uri>"
+                                "<name>Gazebo/DarkGrey</name>"
+                            "</script>"
                         "</material>"
-                        "</visual>"
-                        "</link>";
+                    "</visual>";
         }
     }
 
 
-    xml << "</model>"
-            "</sdf>";
+    xml << "</link>"
+           "</model>"
+           "</sdf>";
 
     sdf::SDF butterSDF;
     butterSDF.SetFromString(xml.str());
